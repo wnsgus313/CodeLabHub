20510fafa20b4c222551fab9c44105da46b58c8b
Junhyeon <wnsgus313@gmail.com>
2022-05-15 23:59:08 +0900

    submission 구현

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { LabProvider, Dependency } from './lab';
+import { LabProvider, Dependency } from './treeView';
 
 export function activate(context: vscode.ExtensionContext) {
 	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
@@ -15,30 +15,61 @@ export function activate(context: vscode.ExtensionContext) {
 		labProvider.refresh();
 	}));
 
-	// 교수가 문제 업로드
+	// admin이 문제 업로드
 	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.uploadProblem', () => {
 		console.log('command : uploadProblem');
 		labProvider.refresh();
 	}));
 
-	// 교수가 문제 삭제
+	// admin이 문제 삭제
 	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.deleteProblem', () => {
 		console.log('command : deleteProblem');
 		labProvider.refresh();
 	}));
 
-	// 교수가 Lab 업로드
+	// admin, student 문제, 코드들 가져오기
+	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.fetchProblem', () => {
+		console.log('command : fetchProblem');
+		labProvider.refresh();
+	}));
+
+	// admin이 Lab 업로드
 	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.makeLab', () => {
 		console.log('command : makeLab');
 		labProvider.refresh();
 	}));
 
-	// 교수가 lab 삭제
+	// admin이 lab 삭제
 	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.deleteLab', () => {
 		console.log('command : deleteLab');
 		labProvider.refresh();
 	}));
 
+	// admin이 멤버 초대
+	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.inviteMember', () => {
+		console.log('command : inviteMember');
+		labProvider.refresh();
+	}));
+
+	// admin이 멤버 삭제
+	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.deleteMember', () => {
+		console.log('command : deleteMember');
+		labProvider.refresh();
+	}));
+
+	// admin이 TA초대
+	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.inviteTA', () => {
+		console.log('command : inviteTA');
+		labProvider.refresh();
+	}));
+
+
+
+	// admin, student가 Code 제출 && 채점
+	context.subscriptions.push(vscode.commands.registerCommand('codelabhub.submitCode', () => {
+		console.log('command : submitCode');
+		labProvider.refresh();
+	}));	
 }
 
 export function deactivate() {}
--- src/treeView.ts
+++ src/treeView.ts
@@ -36,15 +36,22 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
 		}
         const labJsonPath = path.join(this.workspaceRoot, 'labs.json'); // labs.json 경로
         console.log(element);
+        
 		if (element) {
             if(element.contextValue === 'labs'){
                 return Promise.resolve(this.makeFunction(element.order, element.role)); // 몇번째 lab인지 알 수 있도록
             }
             else if(element.contextValue?.substring(0,8) === 'problems'){
-                return Promise.resolve(this.makeProblem(labJsonPath, element.order));
+                return Promise.resolve(this.makeProblem(labJsonPath, element.order, element.role));
             }
             else if(element.contextValue?.substring(0, 7) === 'members'){
-                return Promise.resolve(this.makeMember(labJsonPath, element.order));
+                return Promise.resolve(this.makeMember(labJsonPath, element.order, element.role));
+            }
+            else if(element.contextValue?.substring(0, 11) === 'submissions'){
+                return Promise.resolve(this.makeSubmissionProblem(labJsonPath, element.order, element.role));
+            }
+            else if(element.contextValue?.substring(0, 18) === 'submission_problem'){
+                return Promise.resolve(this.makeSubmission(labJsonPath, element.order, element.role, element.label));
             }
 
 			return Promise.resolve([]);
@@ -61,7 +68,6 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
 
     // lab 추가
 	private makeLab(labJsonPath: string): Dependency[] {
-        console.log("🚀 ~ file: lab.ts ~ line 45 ~ LabProvider ~ makeLab ~ labPath", labJsonPath);
 		if (this.pathExists(labJsonPath)) {
 			const labJson = JSON.parse(fs.readFileSync(labJsonPath, 'utf-8'));
             const roles = labJson['roles'];
@@ -72,8 +78,6 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
 
             let labs:string[] = [];
             for(let i=0; i<Object.keys(labJson['labs']).length; i++){
-                console.log("🚀 ~ file: lab.ts ~ line 60 ~ LabProvider ~ makeLab ~ i", i);
-
                 labs.push(labJson['labs'][i]['lab']);
             }
             this.info.update('lab_count', Object.keys(labJson['labs']).length); // lab의 개수 저장
@@ -86,19 +90,40 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
     }
 
     // 문제 추가
-    private makeProblem(labJsonPath: string, order: number | any): Dependency[] {
+    private makeProblem(labJsonPath: string, order: number | any, role: string | any): Dependency[] {
         if (this.pathExists(labJsonPath)) {
 			const labJson = JSON.parse(fs.readFileSync(labJsonPath, 'utf-8'));
             console.log('makeProblem');
-			const parseProblem = (problem: string): Dependency => {
-				return new Dependency(problem, vscode.TreeItemCollapsibleState.None, 'problem_'+labJson['roles'][order], labJson['roles'][order]); // problem들 가져오기
-			};
+			const parseProblem = (problem: string, index: number): Dependency => {
+                return new Dependency(problem, vscode.TreeItemCollapsibleState.None, 'problem_' + labJson['roles'][order], order, labJson['roles'][order], labJson['labs'][order]['problems'][index]['evaluation']); // problem들 가져오기
+            };
 
             let problems:string[] = [];
             for(let i=0; i<Object.keys(labJson['labs'][order]['problems']).length; i++){
-                problems.push(labJson['labs'][order]['problems'][i]);
+                problems.push(labJson['labs'][order]['problems'][i]['title']);
             }
-            let res = problems.map(lab => parseProblem(lab));
+            let res = problems.map((problem, index) => parseProblem(problem, index));
+
+			return res;
+		} else {
+			return [];
+		}
+    }
+
+    // 교수 submission 하위 문제 추가
+    private makeSubmissionProblem(labJsonPath: string, order: number | any, role: string | any): Dependency[] {
+        if (this.pathExists(labJsonPath)) {
+			const labJson = JSON.parse(fs.readFileSync(labJsonPath, 'utf-8'));
+            console.log('makeSubmissionProblem');
+			const parseProblem = (problem: string, index: number): Dependency => {
+                return new Dependency(problem, vscode.TreeItemCollapsibleState.Collapsed, 'submission_problem_' + labJson['roles'][order], order, labJson['roles'][order]); // problem들 가져오기
+            };
+
+            let problems:string[] = [];
+            for(let i=0; i<Object.keys(labJson['labs'][order]['problems']).length; i++){
+                problems.push(labJson['labs'][order]['problems'][i]['title']);
+            }
+            let res = problems.map((problem, index) => parseProblem(problem, index));
 
 			return res;
 		} else {
@@ -112,7 +137,7 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
         let res: Dependency[] = [];
         if(role === 'admin'){
             res.push(new Dependency("Problem", vscode.TreeItemCollapsibleState.Collapsed, 'problems_'+role, order, role));
-            res.push(new Dependency("Member", vscode.TreeItemCollapsibleState.Collapsed, 'members', order, role));
+            res.push(new Dependency("Member", vscode.TreeItemCollapsibleState.Collapsed, 'members_' + role, order, role));
             res.push(new Dependency("Submission", vscode.TreeItemCollapsibleState.Collapsed, 'submissions', order, role));
             res.push(new Dependency("Class chat", vscode.TreeItemCollapsibleState.None, 'chat', order, role));
             res.push(new Dependency("TA", vscode.TreeItemCollapsibleState.None, "ta", order, role));
@@ -120,7 +145,7 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
         else if(role === 'student'){
             res.push(new Dependency("Problem", vscode.TreeItemCollapsibleState.Collapsed, 'problems_' + role, order, role));
             res.push(new Dependency("Member", vscode.TreeItemCollapsibleState.Collapsed, 'members_' + role, order, role));
-            res.push(new Dependency("Evaluation", vscode.TreeItemCollapsibleState.Collapsed, 'evaluations', order, role));
+            // res.push(new Dependency("Evaluation", vscode.TreeItemCollapsibleState.Collapsed, 'evaluations', order, role));
             res.push(new Dependency("Class chat", vscode.TreeItemCollapsibleState.None, 'chat', order, role));
             res.push(new Dependency("TA", vscode.TreeItemCollapsibleState.None, "ta", order, role));
         }
@@ -129,19 +154,20 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
     }
 
     // 멤버 추가
-    private makeMember(labJsonPath: string, order: number | any){
+    private makeMember(labJsonPath: string, order: number | any, role: string | any){
         if (this.pathExists(labJsonPath)) {
 			const labJson = JSON.parse(fs.readFileSync(labJsonPath, 'utf-8'));
             console.log('makeMember');
-			const parseMember = (member: string): Dependency => {
-				return new Dependency(member, vscode.TreeItemCollapsibleState.None, 'member_'+labJson['roles'][order], labJson['roles'][order]); // problem들 가져오기
+            
+			const parseMember = (member: string, index: number): Dependency => {
+				return new Dependency(member, vscode.TreeItemCollapsibleState.None, 'member_' + role, order, role, labJson['labs'][order]['members'][index]['role']); // problem들 가져오기
 			};
 
             let members:string[] = [];
             for(let i=0; i<Object.keys(labJson['labs'][order]['members']).length; i++){
-                members.push(labJson['labs'][order]['members'][i]);
+                members.push(labJson['labs'][order]['members'][i]['name']);
             }
-            let res = members.map(member => parseMember(member));
+            let res = members.map((member, index) => parseMember(member, index));
 
 			return res;
 		} else {
@@ -149,6 +175,37 @@ export class LabProvider implements vscode.TreeDataProvider<Dependency> {
 		}
     }
 
+    // 학생들 submission된거 확인
+    private async makeSubmission(labJsonPath: string, order: number | any, role: string | any, label: string | any){
+        if (this.pathExists(labJsonPath)) {
+			const labJson = JSON.parse(fs.readFileSync(labJsonPath, 'utf-8'));
+            console.log('makeSubmission');
+
+            // submission order 구하기
+            let sync = async () => {
+                for(let i=0; i<Object.keys(labJson['labs'][order]['problems']).length; i++){
+                    if(labJson['labs'][order]['problems'][i]["title"] === label){
+                        return i;
+                    }
+                }
+            };
+            let submissionOrder: number | any = await sync();
+
+			const parseMember = (member: string, index: number): Dependency => {
+				return new Dependency(member, vscode.TreeItemCollapsibleState.None, 'member_' + role, order, role, labJson['labs'][order]['problems'][submissionOrder]['submission'][index]['evaluation']); // problem들 가져오기
+			};
+
+            let members:string[] = [];
+            for(let i=0; i<Object.keys(labJson['labs'][order]['problems'][submissionOrder]['submission']).length; i++){
+                members.push(labJson['labs'][order]['problems'][submissionOrder]['submission'][i]['name']);
+            }
+            let res = members.map((member, index) => parseMember(member, index));
+
+			return res;
+		} else {
+			return [];
+		}
+    }
 }
 
 export class Dependency extends vscode.TreeItem {
